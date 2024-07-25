2596ce0cf97b906bd920f5a05a2ac3948ede7e63
JENIPHER <jeniperkuki@gmail.com>
2024-07-26 01:21:32 +0300

    swagger edit

--- musren_api/apis/getStatuses/status.controller.js
+++ musren_api/apis/getStatuses/status.controller.js
@@ -5,49 +5,6 @@ const { getStatusData } = require("./status.services");
  * /status:
  *   get:
  *     description: Get status data from Onphone Provider
- *     parameters:
- *       - in: query
- *         name: doneDate
- *         schema:
- *           type: string
- *         required: true
- *         description: The date when the callback was done
- *       - in: query
- *         name: submitDate
- *         schema:
- *           type: string
- *         required: true
- *         description: The date when the message was submitted
- *       - in: query
- *         name: errorCode
- *         schema:
- *           type: string
- *         required: false
- *         description: Error code if any
- *       - in: query
- *         name: shortMessage
- *         schema:
- *           type: string
- *         required: true
- *         description: The short message content
- *       - in: query
- *         name: status
- *         schema:
- *           type: string
- *         required: true
- *         description: The status of the callback
- *       - in: query
- *         name: messageID
- *         schema:
- *           type: string
- *         required: true
- *         description: The ID of the message
- *       - in: query
- *         name: mobile
- *         schema:
- *           type: string
- *         required: true
- *         description: The mobile number associated with the callback
  *     responses:
  *       200:
  *         description: Success
